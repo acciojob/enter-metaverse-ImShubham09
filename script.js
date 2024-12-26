64c679812005e0124fa5a13aeac211a02f0eb986
ImShubham09 <singhashubham612@gmail.com>
2024-12-26 19:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+//one way
+document.addEventListener("DOMContentLoaded", () => {
+    const statusParagraph = document.getElementById("status");
+    const enterButton = document.getElementById("enterBtn");
+
+    enterButton.addEventListener("click", () => {
+        statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+    });
+});
+
+//second way
+document.getElementById("enterBtn").onclick = function () {
+    document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
+};
